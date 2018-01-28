cd502756f22d36212957cd210a2f8ae36992d340
github username <github email address>
2018-01-28 17:28:43 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  constructor()
+  {
+    super()
+    this.state={
+      username:"",
+      hometown:""
+    }
+  }
+
+  // this.props.store.dispatch({
+  //   type: 'ADD_USER',
+  // });
+  handleInput = (e) =>
+  {
+    switch(e.target.name)
+    {
+      case "hometown":
+        this.setState({hometown: e.target.value})
+        break;
+      case "username":
+        this.setState({username: e.target.value})
+        break;
+      default:
+        return console.log("Nothing")
+    }
+  }
+    dispatchSubmit = (e) =>
+    {
+      this.props.store.dispatch({
+        type: 'ADD_USER',
+        user: this.state
+      });
+    }
+
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.dispatchSubmit}>
+          <input type="text" name="hometown" onChange={this.handleInput} placeholder="hometown" />
+          <input type="text" name="username" onChange={this.handleInput} placeholder="username" />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,26 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+    users: [
+  ]
+}, action){
+
+  switch(action.type)
+  {
+    case "ADD_USER":
+      return {users: state.users.concat(action.user)};
+    default:
+      return state
+  }
+  console.log(state)
 }
+// xport default function shoppingListItemReducer(state = {
+//   items: []
+// }, action) {
+//   switch(action.type) {
+//
+//     case 'INCREASE_COUNT':
+//       return state.items.concat(state.items.length + 1);
+//
+//     default:
+//       return state;
+//   }
+// }
