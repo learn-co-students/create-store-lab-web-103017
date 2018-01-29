2eba5db0dee1ecb98b696841e20599c81754888f
dansbands <dansbands@hotmail.com>
2018-01-29 16:49:01 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      hometown: '',
+    }
+  }
+
+
+  handleUsernameChange = event => {
+    console.log('handlingChange', event.target.value);
+    console.log('handlingChangeId', event.target.id);
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handleHometownChange = event => {
+    console.log('handlingChange', event.target.value);
+    console.log('handlingChangeId', event.target.id);
+    this.setState({
+      hometown: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    console.log('handlingSubmit');
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state,
+    })
+  }
+
   render() {
+
     return(
       <div>
-        user input
+        <form onSubmit={((event) => this.handleOnSubmit(event))} >
+          <label>Username:</label>
+          <input type="text" id="username" onChange={(event) => this.handleUsernameChange(event)} ></input>
+          <label>Hometown:</label>
+          <input type="text" id="hometown" onChange={((event) => this.handleHometownChange(event))} ></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,14 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []
+}, action){
+  console.log('the state is', state);
+  console.log('the action is', action);
+  switch (action.type) {
+    case 'ADD_USER':
+      console.log('Adding User', action.user);
+      return Object.assign({}, state, state.users.push(action.user))
+    default:
+      return state;
+  }
+
 }
